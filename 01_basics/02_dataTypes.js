7bb5f57c4d4364e562db1cc70c469a01a3f2aa0a
Payal Baghel <payalkumaribaghel12@gmail.com>
2026-08-22 14:20:03 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict";  // treat all JS code as newer version
+
+//alert('payal baghel');   // we are using node.js not browser
+console.log(3+3)
+
+let name="payal"
+let age=19
+let isNum=false;
+
+//  number  => 2 to 53
+//  bigint
+//  string  => ""
+//  boolean => true/false
+//  null    => standalone value
+//  undefined=>
+//  symbol  => unique
+//  typeof
+console.log(typeof name);
+console.log(typeof null);
+
