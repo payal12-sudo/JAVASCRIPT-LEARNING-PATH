d76f1a4279e39d1491b01a0bc5b530996d555692
Payal Baghel <payalkumaribaghel12@gmail.com>
2026-08-23 06:05:21 +0000

    converiontypes

---
+++ 01_basics/03_conversionType.js
@@ -0,0 +1,26 @@
+"use strict";
+let score="12"
+
+
+console.log(typeof score)
+console.log(typeof(score))
+let value=Number(score)
+console.log(typeof value)
+console.log(value)
+
+//"33"=>33
+//"33abc"=>NaN (type if number)
+//true=>1 and false=>0
+
+
+let isLoggedIn="hi";
+let boolIsLogIn=Boolean(isLoggedIn)
+console.log(boolIsLogIn);
+
+let someNumber = 33;
+
+let stringNum = String(someNumber);
+
+console.log(stringNum);
+console.log(typeof stringNum);
+
